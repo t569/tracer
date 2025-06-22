c67b656f03acfd2547d40a5e3e814d94314166bc
tim569 <timothyobrike@gmail.com>
2025-06-22 22:32:23 +0100

    got an image up and rendering

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
     
     // image
@@ -5,18 +8,35 @@ fn main() {
     let image_height = 256;
     let image_width = 256;
 
-    
+    // write this all to a file
+
+    // first put all of this into a string
+    let mut image_data = String::new();
+
+    image_data.push_str("P3\n");
+    image_data.push_str(&format!("{} {}\n", image_width, image_height));
+    image_data.push_str("255\n");
+
     for i in 0..image_height{
         for j in 0..image_width{
             // Generate a pixel value based on its position
             let r = ((i as f32 / image_height as f32) * 255.0) as u8;
             let g = ((j as f32 / image_width as f32) * 255.0) as u8;
-            let b = 0.0;
-            
-            // // Print the pixel value in grayscale format
-            print!("{:3} {:3} {:3} ", r, g, b);
-            
+            let b = 0.0 as u8; // No blue component
+                    
+            image_data.push_str(&format!("{} {} {} ", r, g, b));
 
         }
     }
+
+    write_to_file("renders/image.ppm", &image_data);
+
+}
+
+
+fn write_to_file(filename: &str, data: &str) {
+    let mut file = File::create(filename).expect("Unable to create file");
+    file.write_all(data.as_bytes()).expect("Unable to write data");
+
+
 }
