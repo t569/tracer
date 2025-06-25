eaa320f6ff150fc319ec62cb9c154b9a33e02dae
tim569 <timothyobrike@gmail.com>
2025-06-25 01:00:16 +0100

    we have a sphere rendering; fixed the direction issue

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,11 @@ pub fn write_to_file(filename: &str, data: &str) {
 
 pub fn ray_color(ray: &Ray) -> Vec3<f64> {
     // Simple ray color function that returns a gradient based on the ray's direction
-    
+    let centre = Vec3::new(0.0, 0.0, -1.0);
+
+    if hit_sphere(&centre, 0.5, ray){
+        return Vec3::new(1.0, 0.0, 0.0); // Red color if the ray hits the sphere
+    }
 
     let unit_direction = ray.direction().normalize();
 
@@ -26,4 +30,21 @@ pub fn ray_color(ray: &Ray) -> Vec3<f64> {
         (1.0 - t) * 1.0 + t * 0.7, // Interpolating between white and light blue
         (1.0 - t) * 1.0 + t * 1.0, // Interpolating between white and light blue
     )
+}
+
+
+pub fn hit_sphere(center: &Vec3<f64>, radius: f64, ray: &Ray) -> bool {
+    // nahhhh, imma do it my own way
+
+    let oc = center - ray.origin();
+    let unit_ray_direction = ray.direction().normalize();
+
+    let a = unit_ray_direction.dot(&unit_ray_direction);
+    let b = -2.0 * oc.dot(&unit_ray_direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant >= 0.0
+
+
 }
